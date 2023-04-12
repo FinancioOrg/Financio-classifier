83adbabbefefb62e8a3dca63f29bfbc42dd55e89
tymob0 <87370293+tymob0@users.noreply.github.com>
2023-04-12 09:31:02 +0200

    added print results

--- src/classifier.py
+++ src/classifier.py
@@ -3,7 +3,7 @@ from categoriesDAL import fetch_categories
 
 def classify_article(text):
     categories = fetch_categories()
-    print(categories)
     pipe = pipeline(model="facebook/bart-large-mnli")
     result = pipe(text,candidate_labels=categories)
+    print(result)
     return result
