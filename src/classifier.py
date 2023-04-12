53e11c0e86d4fbedabfdc84f126dd1240ed77673
tymob0 <87370293+tymob0@users.noreply.github.com>
2023-04-12 10:19:44 +0200

    switched to roberta-large-mnli (faster model)

--- src/classifier.py
+++ src/classifier.py
@@ -3,7 +3,18 @@ from categoriesDAL import fetch_categories
 
 def classify_article(text):
     categories = fetch_categories()
-    pipe = pipeline(model="facebook/bart-large-mnli")
+    pipe = pipeline("zero-shot-classification", model="roberta-large-mnli")
     result = pipe(text,candidate_labels=categories)
     print(result)
     return result
+
+
+# from transformers import pipeline
+# from categoriesDAL import fetch_categories
+
+# def classify_article(text):
+#     categories = fetch_categories()
+#     print(categories)
+#     pipe = pipeline(model="facebook/bart-large-mnli")
+#     result = pipe(text,candidate_labels=categories)
+#     return result
