b7afca289fa549789f585822ba040d5106f80140
tymob0 <87370293+tymob0@users.noreply.github.com>
2023-05-30 11:59:23 +0200

    updated ml pipeline

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 def summarize_article(text):
-    sum_pipeline = pipeline("summarization",model="sshleifer/distilbart-cnn-12-6")
+    sum_pipeline = pipeline("summarization",model="sshleifer/distilbart-cnn-12-6", device='cpu')
     result = sum_pipeline(text)
     print(result)
     return result
